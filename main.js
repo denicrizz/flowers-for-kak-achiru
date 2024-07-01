62be2f591b031a51e34d72b9585fe944d001616b
Deni kristanto <crizzdeni4@gmail.com>
2024-07-01 17:51:17 +0700

    penambahan music

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+document.addEventListener('click', function() {
+        var audio = document.getElementById('background-music');
+        audio.play();
+      }, { once: true });
